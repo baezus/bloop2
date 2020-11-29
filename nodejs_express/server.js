e960717552a78eedadd6f29359e8ff1dd2a5b227
Baezus <elias@baez.us>
2020-11-29 13:29:14 -0500

    Got Passport-Local up and running as well as MongoDB/Mongoose & Express. Next I will have to refactor them, after researching the sendbird process.

--- nodejs_express/server.js
+++ nodejs_express/server.js
@@ -1,8 +1,11 @@
+//Express Config
+
 const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
 
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const expressSession = require('express-session')({
   secret: 'secret',
@@ -10,9 +13,87 @@ const expressSession = require('express-session')({
   saveUninitialized: false
 });
 
+//Passport Init
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSession);
 
+//Server start
 const port = process.env.PORT || 2737;
 app.listen(port, () => console.log('Server running on port ' + port));
+
+//Mongoose config & connect
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+mongoose.connect(process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+  username: String,
+  password: String
+});
+
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+//Passport Local Authentication
+
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+//Routes
+
+const connectEnsureLogin = require('connect-ensure-login');
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local', 
+  (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.redirect('/login?info=' + info);
+    }
+
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      return res.redirect('/');
+    });
+
+  })(req, res, next);
+});
+
+app.get('/login',
+  (req, res) => res.sendFile('html/login.html',
+  { root: __dirname })
+);
+
+app.get('/',
+connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.sendFile('html/index.html', {root: __dirname}));
+
+app.get('/private',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.sendFile('html/private.html', {root: __dirname}));
+
+app.get('/user',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({ user: req.user }));
+
+
+//Register some dummy users
+// UserDetails.register({username:'paul', active: false}, 'paul');
+// UserDetails.register({username:'jay', active: false}, 'jay');
+// UserDetails.register({username:'roy', active: false}, 'roy');
