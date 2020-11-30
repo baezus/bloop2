2057a6347ac7d22ee00c8650834841f5d3113a10
Baezus <elias@baez.us>
2020-11-30 07:12:25 -0500

    Finished React-Redux boilerplate, a little bit.

--- bloop/src/components/App.js
+++ bloop/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from 'features/filters/Footer'
 import AddProfile from 'features/profiles/AddProfile'
-import VisibleTodoList from 'features/profiles/VisibleProfileList'
+import VisibleProfileList from 'features/profiles/VisibleProfileList'
 
 const App = () => (
   <div>
