45c20e037bbb6a026a3d0e4c072e9d50fa360062
Baezus <elias@baez.us>
2020-12-02 22:44:18 -0500

    Deleted some extra comments in the App.js page.

--- bloop/src/components/App.js
+++ bloop/src/components/App.js
@@ -6,42 +6,8 @@ import Dashboard from './Dashboard';
 import Blah from './Blah';
 import '../styles/bloop.scss'
 
-// const socket = io.connect('http://localhost:2737');
 
 class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = { msg: "", chat: [], nickname: ""};
-  // }
-
-  // componentDidMount() {
-  //   socket.on('chat message', ({ nickname, msg }) => {
-  //     this.setState({
-  //       chat: [...this.state.chat, { nickname, msg }]
-  //     });
-  //   });
-  // }
-
-  // onTextChange = e => {
-  //   this.setState({ [e.target.name]: e.target.value });
-  // };
-
-  // onMessageSubmit = () => {
-  //   const { nickname, msg } = this.state;
-  //   socket.emit('chat message', { nickname, msg});
-  //   this.setState({ msg: '' });
-  // };
-
-  // renderChat() {
-  //   const { chat } = this.state;
-  //   return chat.map(({ nickname, msg }, idx) => (
-  //     <div key={idx}>
-  //       <span style={{ color: 'green' }}>{nickname} </span>
-
-  //       <span>{msg}</span>
-  //     </div>
-  //   ));
-  
 
   render() {
     return (
