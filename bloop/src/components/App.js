f512771f54ecbdbc32e80620d08c8df71176d5b4
Baezus <elias@baez.us>
2020-12-03 14:26:27 -0500

    More styling -- breadcrumb done, and link hover:active:etc. behavior is mostly done now.

--- bloop/src/components/App.js
+++ bloop/src/components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
                   <div className="columns is-centered">
                     <div className="column">
                       <h1 className="title">Bloop</h1>
-                      <h3 className="subtitle is-3">Who's out there?</h3>
+                      <h3 className="subtitle is-3">Is anyone there?</h3>
                         
                     </div>
                     </div>
