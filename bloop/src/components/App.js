3daa48dee3946ffa5e4e708182b26b55baef5438
Baezus <elias@baez.us>
2020-12-03 19:51:50 -0500

    Cleaned up some and added 'with History' to the Auth login.

--- bloop/src/components/App.js
+++ bloop/src/components/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link, Redirect, withRouter } from 'react-router-dom';
 import Passport from './Passport';
-// import LoginPage from './LoginPage';
-// import AuthStatus from './AuthStatus';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import OneUser from './ShowUser';
@@ -12,73 +10,6 @@ import Header from './Header';
 import Footer from './Footer';
 import '../styles/bloop.scss';
 import { useAuth0 } from '@auth0/auth0-react';
-
-// const AuthService = {
-//   isAuthenticated: false,
-//   authenticate(cb) {
-//     this.isAuthenticated = true 
-//     setTimeout(cb, 100)
-//   },
-//   logout(cb) {
-//     this.isAuthenticated = false 
-//     setTimeout(cb, 100)
-//   }
-// };
-
-// const AuthStatus = withRouter(({ history }) => (
-//   AuthService.isAuthenticated ? (
-//     <p>
-//       Welcome! <button onClick={()=>{
-//         AuthService.logout(() => history.push('/'))
-//       }}>Sign Out</button>
-//     </p>
-//   ) : (
-//     <p>You are not logged in.</p>
-//   )
-// ));
-
-// const SecretRoute = ({ component: Component, ...rest }) => (
-//   <Route {...rest} render={(props) => (
-//     AuthService.isAuthenticated === true
-//     ? <Component {...props} />
-//     : <Redirect to={{
-//       pathname: '/login',
-//       state: { from: props.location }
-//     }} />
-//   )}/>
-// );
-
-// class LoginPage extends React.Component { 
-//   state = {
-//     redirectToPreviousRoute: false
-//   };
-
-//   // login = () => {
-//   //   AuthService.authenticate(() => {
-//   //     this.setState({ redirectToPreviousRoute: true });
-//   //   });
-//   // };
-
-//   render() {
-//     const { from } = this.props.location.state || { from: { pathname: "/" }};
-//     const { redirectToPreviousRoute } = this.state;
-
-//     if (redirectToPreviousRoute) {
-//       return <Redirect to={from}/>;
-//     }
-
-//     return (
-//       <div>
-//         <p>You must be logged in to view the page at {from.pathname}</p>
-//         <LoginButton/>
-//       </div>
-//     );
-//   }
-// }
-
-
-
-
 
 function App () {
 
--- bloop/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import profilesReducer from '../features/profiles/profilesSlice'
-import visibilityFilterReducer from '../features/filters/filtersSlice'
-
-export default combineReducers({
-  profiles: profilesReducer,
-  visibilityFilter: visibilityFilterReducer
-});
